dfcc5f02621f8df9a8cc0f9fde707c0cf68a8d59
yochidros <mm9.movement.trb@gmail.com>
2020-04-23 11:33:17 +0900

    Initial Commit

---
+++ TwemojiKit/TwemojiKit.h
@@ -0,0 +1,19 @@
+//
+//  TwemojiKit.h
+//  TwemojiKit
+//
+//  Created by miyazawa yoshiki on 2020/04/23.
+//  Copyright © 2020 yochidros. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TwemojiKit.
+FOUNDATION_EXPORT double TwemojiKitVersionNumber;
+
+//! Project version string for TwemojiKit.
+FOUNDATION_EXPORT const unsigned char TwemojiKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TwemojiKit/PublicHeader.h>
+
+
