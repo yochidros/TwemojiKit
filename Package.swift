371f5ae39217b05299d14c80dbf31f078cc552fd
yochidros <mm9.movement.trb@gmail.com>
2021-02-02 13:35:07 +0900

    fix: package swift

--- Package.swift
+++ Package.swift
@@ -5,21 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "TwemojiKit",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TwemojiKit",
-            targets: ["TwemojiKit"]),
+            targets: ["TwemojiKit"])
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TwemojiKit",
-            path: "Sources",
-            dependencies: []),
-        .testTarget(
-            name: "TwemojiKitTests",
-            dependencies: ["TwemojiKit"]),
+            path: "Sources")
+    ],
+    swiftLanguageVersions: [
+        .version("5")
     ]
 )
