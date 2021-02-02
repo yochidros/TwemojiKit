06b66cb630c883893047012db48db3dc010a4aba
yochidros <mm9.movement.trb@gmail.com>
2021-02-02 13:40:31 +0900

    fix: package for resources

--- Package.swift
+++ Package.swift
@@ -19,7 +19,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TwemojiKit",
-            path: "Sources")
+            path: "Sources",
+            resources: [.copy("Core")]
+        )
     ],
     swiftLanguageVersions: [
         .version("5")
