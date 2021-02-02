76fe0658bf76d1d0965d2055db9d2110b4aee437
yochidros <mm9.movement.trb@gmail.com>
2021-02-02 13:29:13 +0900

    fix: package

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TwemojiKit",
+            path: "Sources",
             dependencies: []),
         .testTarget(
             name: "TwemojiKitTests",
